ff1081b7f9fbb43cf3d9a76d66f0f063af15e24d
ahnge <ahnge226@gmail.com>
2022-10-21 12:13:38 +0630

    new test

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from django.test import LiveServerTestCase
 
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
@@ -14,3 +14,25 @@ class HostTest(LiveServerTestCase):
 
         driver.get(self.live_server_url)
         assert "Hello, world!" in driver.title
+
+
+class LoginFormTest(LiveServerTestCase):
+
+    def testform(self):
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(chrome_options=options)
+
+        driver.get(('%s%s' % (self.live_server_url, '/accounts/login/')))
+
+        user_name = driver.find_element_by_name('username')
+        user_password = driver.find_element_by_name('password')
+
+        submit = driver.find_element_by_id('submit')
+
+        user_name.send_keys('admin')
+        user_password.send_keys('admin')
+
+        submit.send_keys(Keys.RETURN)
+
+        assert 'admin' in driver.page_source
